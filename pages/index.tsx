cd95b0408af7c434dbb1412c8e7481d8319e991a
Yurii Khomin <yurii.khomin@gmail.com>
2021-07-14 17:20:28 +0300

    fin code

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,14 @@ export default function Home() {
     setDeadline(0);
   };
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
   return (
     <div className="App">
       <div className="header">
@@ -45,7 +53,7 @@ export default function Home() {
       </div>
       <div className="todoList">
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} />;
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
       </div>
     </div>
